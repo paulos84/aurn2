613465285bff8d5ec0258afb43c05ff78b41a502
Paul Davis <pjdavis@gmx.com>
2018-01-16 21:05:23 +0100

    add APIview mixins and add Data model views

--- api/urls.py
+++ api/urls.py
@@ -2,11 +2,12 @@ from django.conf.urls import url
 from rest_framework.urlpatterns import format_suffix_patterns
 from api import views
 
+
 urlpatterns = [
     url(r'^sites/$', views.SiteList.as_view()),
     url(r'^sites/(?P<pk>[0-9]+)/$', views.SiteDetail.as_view()),
     url(r'^data/$', views.DataList.as_view()),
-    #url(r'^data/(?P<site>\w+)/$', views.SiteDetail.as_view()),
+    url(r'^data/(?P<code>\w+)/$', views.AllSiteData.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- api/views.py
+++ api/views.py
@@ -3,7 +3,6 @@ from .models import Data, Site
 from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import mixins
 from rest_framework import generics
 
 
@@ -14,42 +13,33 @@ class SiteList(APIView):
         serializer = SiteSerializer(sites, many=True)
         return Response(serializer.data)
 
-class SiteDetail(mixins.RetrieveModelMixin,
-                    mixins.UpdateModelMixin,
-                    mixins.DestroyModelMixin,
-                    generics.GenericAPIView):
+
+class SiteDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Site.objects.all()
     serializer_class = SiteSerializer
 
     def get(self, request, pk):
         return self.retrieve(request, pk=pk)
 
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
+    def put(self, request, pk):
+        return self.retrieve(request, pk=pk)
 
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
-
+    def delete(self, request, pk):
+        return self.retrieve(request, pk=pk)
 
 
 class DataList(APIView):
 
     def get(self, request, format=None):
-        data_list = Data.objects.all()
-        serializer = DataSerializer(data_list, many=True)
+        queryset = Site.objects.all()
+        serializer = DataSerializer(queryset, many=True)
         return Response(serializer.data)
 
 
 class AllSiteData(APIView):
 
-    def get_objects(self, code):
-        try:
-            return Data.objects.filter(site__code=code)
-        except Site.DoesNotExist:
-            raise Http404
-
-    def get(self, request, site, format=None):
-        site_data = self.get_object(site)
-        serializer = SiteSerializer(site_data, many=True)
+    def get(self, request, code, format=None):
+        queryset = Site.objects.filter(site__code=code)
+        serializer = DataSerializer(queryset, many=True)
         return Response(serializer.data)
 
