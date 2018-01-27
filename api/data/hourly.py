d376590c3b7cb05301a578d12c082a5aac6ef2a8
Paul Davis <pjdavis@gmx.com>
2018-01-27 16:48:39 +0100

    modify data validation function for midnight values

--- api/data/hourly.py
+++ api/data/hourly.py
@@ -12,17 +12,15 @@ def hourly_data(soup, site):
         so2 = row[3].text.replace('\xa0', ' ').split(' ')[0]
         pm25 = row[4].text.replace('\xa0', ' ').split(' ')[0]
         pm10 = row[5].text.replace('\xa0', ' ').split(' ')[0]
-        time = row[6].text[:10] + ' ' + row[6].text[10:]
-        if time.split()[1] == '24:00':
-            time = time.replace('24:00', '00:00')
-        return {'o3': o3, 'no2': no2, 'so2': so2, 'pm25': pm25, 'pm10': pm10, 'time': time}
+        hour = row[6].text[:10] + ' ' + row[6].text[10:]
+        return {'o3': o3, 'no2': no2, 'so2': so2, 'pm25': pm25, 'pm10': pm10, 'time': hour}
 
 
 def validate_data(data_dict):
     """ ensure that stale data is not recorded """
     hour_dt = datetime.now(timezone.utc).replace(microsecond=0, second=0, minute=0)
     current_hour = datetime.strftime(hour_dt, "%d/%m/%Y %H:%M")
-    if data_dict and data_dict['time'] != current_hour:
+    if data_dict['time'] != current_hour and data_dict['time'].split()[1] != '24:00':
         na_values = ['n/a'] * 5 + [current_hour]
         return dict(zip(['o3', 'no2', 'so2', 'pm25', 'pm10', 'time'], na_values))
     else:
