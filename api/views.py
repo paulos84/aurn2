834e13a064846533c60d6342cfd84c506d8bc124
Paul Davis <pjdavis@gmx.com>
2018-02-01 13:39:03 +0100

    add endpoints for date filtering

--- api/views.py
+++ api/views.py
@@ -29,6 +29,25 @@ class RecentSiteData(APIView):
         return Response(serializer.data)
 
 
+class DateData(APIView):
+
+    def get(self, request, date, format=None):
+        """ filter results according to a specified day with format YYYY-MM-DD """
+        uk_date = '{}/{}/{}'.format(*date.split('-')[::-1])
+        queryset = Data.objects.filter(time__contains=uk_date)
+        serializer = DataSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+
+class DateRangeData(APIView):
+
+    def get(self, request, date1, date2, format=None):
+        """ filter results according to a specified date range withs formats YYYY-MM-DD """
+        uk_date1 = '{}/{}/{}'.format(*date1.split('-')[::-1])
+        uk_date2 = '{}/{}/{}'.format(*date2.split('-')[::-1])
+        pass
+
+
 class SiteViewSet(viewsets.ReadOnlyModelViewSet):
 
     queryset = Site.objects.all()
